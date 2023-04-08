5a59237f87708633f0444166b5244c80e8c4b021
Simon Wong <swong@duck.com>
2023-04-08 10:21:49 -0400

    Init modified AstroWind shell

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export const headerData = {
+  links: [
+    {
+      text: 'About',
+      href: '#',
+    },
+    {
+      text: 'Engineering',
+      href: '#',
+    },
+    {
+      text: 'Photography',
+      href: '#',
+    },
+    {
+      text: 'Contact',
+      href: '#',
+    },
+  ],
+};
+
+export const footerData = {
+  socialLinks: [
+    { ariaLabel: 'LLUNA POP', icon: 'tabler:moon', href: 'https://pop.hellolluna.com/pop/simon' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/_szwo' },
+    { ariaLabel: 'LinkedIn', icon: 'tabler:brand-linkedin', href: 'https://www.linkedin.com/in/simonzwong' },
+    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/szwo' },
+  ],
+  footNote: `<p>&copy; 2023 Simon Wong & Wong Exposure</p><p class="text-muted text-xs">Bootstrapped with the AstroWind template. Thank you, onWidget!</p>`,
+};
